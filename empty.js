559c8d684567900464ce3941834e2c326e71922f
hanwool95 <77750865+hanwool95@users.noreply.github.com>
2022-07-21 22:09:10 +0900

    Initial commit

---
+++ empty.js
@@ -0,0 +1,29 @@
+
+// d3.select('body').append("p").text("New paragraph!");
+
+let dataset = [5, 10, 15, 20, 25]
+
+d3.select("body").select("p")
+    .data(dataset)  // 값마다 한 번씩 실행 전달.
+    .enter()        // DOM 선택물 있는지 보고 데이터 전달. 플레이스 홀더 역할 문서요소 생성.
+    .append("p")    // 플레이스 홀더 요소 가져와서 p 추가.
+    .text("New paragraph!")
+
+
+d3.select("body")
+    .select("h1")
+    .data(dataset)
+    .enter()
+    .append("h1")
+    .text(function(d) { return d; })
+
+console.log(d3.selectAll("p"))
+
+d3.select("body")
+    .select("h2")
+    .data(dataset)
+    .enter()
+    .append("h2")
+    .text(function(d) { return d; })
+    // .attr() // HTML 속성 지정.
+    .style("color", "red") // css 프로퍼티 지정
